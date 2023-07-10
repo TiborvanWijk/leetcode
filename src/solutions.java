b6547169d83d7b4a8de3882ba4055dcb2e0f4675
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-10 18:23:34 +0200

    smallest leaf in a tree

--- src/solutions.java
+++ src/solutions.java
@@ -412,11 +412,33 @@ public class solutions {
         return -1;
     }
 
+    public int minDepth(TreeNode root) {
+        if (root == null){
+            return 0;
+        }
+
+
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+
+        if (root.left != null && root.right != null){
+            return Math.min(left, right) + 1;
+        }
+        return Math.max(left, right) + 1;
+
+
+    }
 
 
 
+    public int minEatingSpeed(int[] piles, int h) {
+
+        Arrays.sort(piles);
+
+        return piles[h - (piles.length -1)];
 
 
+    }
 
 
 
