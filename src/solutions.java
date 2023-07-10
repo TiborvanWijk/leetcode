70b26554ec41d47c0c223c85556c6a4dd2ec4604
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-10 16:52:56 +0200

    Initial commit

---
+++ src/solutions.java
@@ -0,0 +1,430 @@
+import java.sql.Struct;
+import java.util.*;
+
+public class solutions {
+    ArrayList<Integer> ans = new ArrayList<>();
+
+//    public ArrayList<Integer> InorderTreversal(Node root) {
+//        if (root == null) return ans;
+//        InorderTreversal(root.left);
+//        System.out.println(root);
+//        InorderTreversal(root.right);
+//        return ans;
+//    }
+
+    public boolean containsDuplicate(int[] nums) {
+
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int number = nums[i];
+            if (seen.contains(number)) return true;
+            seen.add(number);
+        }
+        return false;
+    }
+
+
+    public boolean isAnagram(String s, String t) {
+        char[] sChars = s.toCharArray();
+        char[] tChars = t.toCharArray();
+
+        Arrays.sort(sChars);
+        Arrays.sort(tChars);
+
+
+        return Arrays.equals(sChars, tChars);
+    }
+
+
+//    public int[] twoSum(int[] nums, int target) {
+//
+//        Map<Integer, Integer> list = new HashMap<>();
+//
+//        for(int i = 0; i < nums.length; i++){
+//            if (list.containsKey(nums[i])){
+//                return new int[] {list.get(nums[i]), i};
+//            }
+//            else {
+//                list.put(target - nums[i], i);
+//            }
+//        }
+//        return nums;
+//    }
+
+
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, List<String>> list = new HashMap<>();
+
+        for (String word : strs) {
+            char[] toArray = word.toCharArray();
+            Arrays.sort(toArray);
+            String sortedWord = new String(toArray);
+
+            if (!list.containsKey(sortedWord)) {
+                list.put(sortedWord, new ArrayList<>());
+            }
+            list.get(sortedWord).add(word);
+
+        }
+
+
+        return new ArrayList<>(list.values());
+    }
+
+
+//    public int[] topKFrequent(int[] nums, int k) {
+//        Map<Integer, Integer> list = new HashMap<>();
+//
+//        for (int i = 0; i < nums.length; i++){
+//            int count = list.containsKey(nums[i]) ? list.get(nums[i]) + 1 : 1;
+//            list.put(list.getOrDefault(nums[i], 1, ++));
+//            list.put(nums[i], count);
+//
+//        }
+//        for (int j = 0; j <= k; j++) {
+//
+//            for (int i = 0; i < list.size(); i++) {
+//
+//            }
+//        }
+
+    public int longestConsecutive(int[] nums) {
+
+        Arrays.sort(nums);
+        int ans = 0;
+        int temp = 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] == nums[i + 1] - 1) {
+                temp++;
+                ans = Math.max(temp, ans);
+            } else if (nums[i] == nums[i + 1]) {
+                ans = Math.max(temp, ans);
+            } else {
+                temp = 1;
+                ans = Math.max(temp, ans);
+            }
+
+
+        }
+
+        return ans;
+    }
+
+    public boolean isValid(String s) {
+
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+
+            if (c == '(' || c == '[' || c == '{') {
+                stack.push(c);
+            } else {
+                if (stack.empty()) {
+                    return false;
+                }
+
+                if (stack.peek() == '(' && c == ')') {
+                    stack.pop();
+                } else if (stack.peek() == '[' && c == ']') {
+                    stack.pop();
+                } else if (stack.peek() == '{' && c == '}') {
+                    stack.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        return stack.empty();
+    }
+
+
+//    public int[] dailyTemperatures(int[] temperatures) {
+//        int[] ans = new int[temperatures.length];
+//
+//        for (int i = 0; i < temperatures.length; i++){
+//            for (int j = i + 1; j < temperatures.length; j++){
+//                if (temperatures[i] < temperatures[j]){
+//                    ans[i] = j - i;
+//                    break;
+//                }
+//            }
+//        }
+//
+//        return ans;
+//
+//
+//
+//    }
+//public int[] dailyTemperatures(int[] temperatures) {
+//    int[] ans = new int[temperatures.length];
+//
+//    for (int i = 0; i < temperatures.length; i++){
+//
+//    }
+//
+//    return ans;
+//
+//
+//
+//}
+//    public int[] dailyTemperatures(int[] temp) {
+//        Stack<Integer> stack = new Stack<>();
+//        int[] ans = new int[temp.length];
+//
+//        for (int i = 0; i < temp.length; i++){
+//
+//            while (!stack.empty() && stack.peek() <= temp[i]){
+//                stack.pop();
+//            }
+//
+//            if (!stack.empty()){
+//                ans[i] = stack.peek() - 1;
+//            }
+//
+//            stack.push(i);
+//        }
+//        return ans;
+//    }
+
+    public boolean isPalindrome(String s) {
+        StringBuilder reverse = new StringBuilder();
+        s = s.toLowerCase();
+        for (int i = 0; i < s.length(); i++) {
+            if (Character.isLetterOrDigit(s.charAt(i))) {
+                reverse.append(s.charAt(i));
+            }
+        }
+        return String.valueOf(reverse).equals(String.valueOf(reverse.reverse()));
+    }
+
+    public int[] twoSum(int[] numbers, int target) {
+
+        int[] ans = new int[2];
+        int startIndex = 0;
+        int endIndex = numbers.length - 1;
+
+        while (startIndex < endIndex) {
+
+            if (numbers[startIndex] + numbers[endIndex] > target) {
+                endIndex--;
+            } else if (numbers[startIndex] + numbers[endIndex] < target) {
+                startIndex++;
+            } else {
+                ans[0] = startIndex + 1;
+                ans[1] = endIndex + 1;
+                return ans;
+            }
+        }
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public int singleNumber(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int i = 0; i < nums.length; i++){
+            int count = map.containsKey(nums[i]) ? map.get(nums[i]) + 1 : 1;
+
+            map.put(nums[i], count);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1){
+                ans = entry.getKey();
+                break;
+            }
+        }
+
+        return ans;
+    }
+
+
+
+
+
+
+
+    public int maxProfit(int[] prices) {
+        int leftPointer = 0;
+        int rightPointer = 1;
+        int max = 0;
+        while (rightPointer < prices.length){
+            if (prices[rightPointer] < prices[leftPointer]){
+                leftPointer = rightPointer;
+                rightPointer++;
+            }
+            else if ((prices[rightPointer] - prices[leftPointer]) > max){
+                max = prices[rightPointer] - prices[leftPointer];
+            }
+            else {
+                rightPointer++;
+            }
+        }
+        return max;
+    }
+
+
+    public int lengthOfLongestSubstring(String s) {
+        ArrayList<Character> stack = new ArrayList<>();
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++){
+
+            while (stack.contains(s.charAt(i))){
+                stack.remove(0);
+            }
+            stack.add(s.charAt(i));
+            ans = Math.max(ans, stack.size());
+        }
+        return ans;
+    }
+
+
+
+
+
+//    public boolean checkInclusion(String s1, String s2) {
+//
+//        Map<Character, Integer> s1Array = new HashMap<>();
+//        Map<Character, Integer> s2Array = new HashMap<>();
+//
+//        for (int i = 0; i < s1.length(); i++){
+//            int count = s1Array.containsKey(s1.charAt(i)) ? s1Array.get(s1.charAt(i)) + 1 : 1;
+//            s1Array.put(s1.charAt(i), count);
+//        }
+//        for (int i = 0; i < s2.length(); i++){
+//            int count = s2Array.containsKey(s1.charAt(i)) ? s2Array.get(s1.charAt(i)) + 1 : 1;
+//            s2Array.put(s2.charAt(i), count);
+//        }
+//
+//        for (int i = 0; i < )
+//
+//    }
+
+
+    public int[] plusOne(int[] digits) {
+
+        for (int i = digits.length - 1; i >= 0; i--){
+            if (digits[i] < 9){
+                digits[i]++;
+                return digits;
+            }
+            digits[i] = 0;
+        }
+
+        digits = new int[digits.length + 1];
+        digits[0] = 1;
+
+        return digits;
+    }
+
+
+
+//    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+//        if(list1!=null && list2!=null){
+//            if(list1.val<list2.val){
+//                list1.next=mergeTwoLists(list1.next,list2);
+//                return list1;
+//            }
+//            else{
+//                list2.next=mergeTwoLists(list1,list2.next);
+//                return list2;
+//            }
+//        }
+//        if(list1==null)
+//            return list2;
+//        return list1;
+//    }
+
+
+
+
+
+
+
+
+
+
+
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums.length == 0) return nums;
+        int[] ans = new int[nums.length - (k - 1)];
+
+        for (int i = 0; i <= nums.length - k; i++){
+            int max = nums[i];
+            for (int j = 1; j < k; j++){
+                max = Math.max(max, nums[i + j]);
+            }
+            ans[i] = max;
+        }
+        return ans;
+    }
+
+
+
+
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int middle = low + (high - low) / 2;
+            if (nums[middle] == target) {
+                return middle;
+            }
+            else if (target > nums[middle]) {
+                low = middle + 1;
+            }
+            else if (target < nums[middle]){
+                high = middle - 1;
+            }
+        }
+
+
+        return -1;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
