aa6fcc00a2bab27139ec361e6d37ad5bbff42e6c
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-19 17:18:14 +0200

    isSymetric

--- src/solutions.java
+++ src/solutions.java
@@ -1071,7 +1071,21 @@ public class solutions {
 
     }
 
+    public boolean isSymmetric(TreeNode root) {
 
+        if (root == null) return true;
+
+        return isSymmetricHelper(root.left, root.right);
+    }
+    public boolean isSymmetricHelper(TreeNode left, TreeNode right) {
+
+        if (left == null && right == null) return true;
+
+        if (left == null || right == null) return false;
+
+        return left.val == right.val && isSymmetricHelper(left.left, right.right) && isSymmetricHelper(left.right, right.left);
+
+    }
 
 
 
