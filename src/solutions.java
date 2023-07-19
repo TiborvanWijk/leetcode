b13038462110c80bd3d903f6ab63878cb7925d35
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-19 17:08:43 +0200

    isSameTree

--- src/solutions.java
+++ src/solutions.java
@@ -1061,7 +1061,15 @@ public class solutions {
 
 
 
+    public boolean isSameTree(TreeNode p, TreeNode q) {
 
+        if (p == null && q == null) return true;
+
+        if (p == null || q == null || p.val != q.val) return false;
+
+        return isSameTree(p.right, q.right) && isSameTree(p.left, q.left);
+
+    }
 
 
 
