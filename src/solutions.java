b0d9f50899bc8a0fd91396a6c3d7a8ff75734668
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-22 16:59:29 +0200

    merge

--- src/solutions.java
+++ src/solutions.java
@@ -1209,7 +1209,14 @@ public class solutions {
 
 
 
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
 
+        for (int j = 0, i = m; j < n; j++){
+            nums1[i] = nums2[j];
+            i++;
+        }
+        Arrays.sort(nums1);
+    }
 
 
 
