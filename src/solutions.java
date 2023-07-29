374a39359a6b40d31340eae9d65a672be2f8a08e
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-29 12:16:14 +0200

    hIndex

--- src/solutions.java
+++ src/solutions.java
@@ -1505,7 +1505,22 @@ public class solutions {
 
 
 
-
+    public int hIndex(int[] citations) {
+        int n = citations.length;
+        int[] freq = new int[n + 1];
+        for(int num : citations){
+            if(num > n) freq[n]++;
+            else{
+                freq[num]++;
+            }
+        }
+        int sum = 0;
+        for(int i = n; i >= 0; i--){
+            sum += freq[i];
+            if(sum >= i) return i;
+        }
+        return 0;
+    }
 
 
 
