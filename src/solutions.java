884bafa210362a4cd7c56af4cfae3a06a83f2394
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-30 18:37:56 +0200

    longestCommonPrefix

--- src/solutions.java
+++ src/solutions.java
@@ -1622,7 +1622,7 @@ public class solutions {
                 ans.add(start + "->" + nums[i]);
             }
             else if (start == nums[i]){
-                ans.add("" + start);
+                ans.add(String.valueOf(start));
             }
 
         }
@@ -1732,6 +1732,21 @@ public class solutions {
 
 
 
+
+    public String longestCommonPrefix(String[] strs) {
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int index = 0;
+        while(index < s1.length() && index < s2.length()){
+            if(s1.charAt(index) == s2.charAt(index)){
+                index++;
+            } else {
+                break;
+            }
+        }
+        return s1.substring(0, index);
+    }
 
 
 
