019c400c618fd8656cc0952fe7369c60759447ba
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-30 17:51:34 +0200

    myPow

--- src/solutions.java
+++ src/solutions.java
@@ -1710,4 +1710,41 @@ public class solutions {
     }
 
 
+    public double myPow(double x, long n) {
+        if(n < 0) {
+            x = 1 / x;
+            n = -n;
+        }
+
+        double result = 1;
+        double current_product = x;
+
+        while(n > 0) {
+            if(n % 2 == 1) {
+                result = result * current_product;
+            }
+            current_product = current_product * current_product;
+            n = n / 2;
+        }
+
+        return result;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
