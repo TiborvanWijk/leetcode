9b54f830af9ec344c884e9472d05a46c400de975
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-30 19:00:14 +0200

    romanToInt

--- src/solutions.java
+++ src/solutions.java
@@ -1751,7 +1751,30 @@ public class solutions {
 
 
 
+    public int romanToInt(String s) {
 
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('M', 1000);
+        map.put('D', 500);
+        map.put('C', 100);
+        map.put('L', 50);
+        map.put('X', 10);
+        map.put('V', 5);
+        map.put('I', 1);
+
+        int ans = 0;
+
+        for (int i = 0; i < s.length(); i++){
+            if (i < s.length() - 1 && map.get(s.charAt(i)) < map.get(s.charAt(i + 1))){
+                ans -= map.get(s.charAt(i));
+            }
+            else {
+                ans += map.get(s.charAt(i));
+            }
+        }
+
+        return ans;
+    }
 
 
 
