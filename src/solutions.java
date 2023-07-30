c9340314bd505e17fe05be27ae23c85787a05d0b
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-30 15:17:06 +0200

    getMinimumDifference

--- src/solutions.java
+++ src/solutions.java
@@ -1665,7 +1665,31 @@ public class solutions {
 
 
 
+    private int minDiff;
+    private TreeNode prev;
 
+    public int getMinimumDifference(TreeNode root) {
+        minDiff = Integer.MAX_VALUE;
+        prev = null;
+        inOrderTraversal(root);
+        return minDiff;
+    }
+
+    private void inOrderTraversal(TreeNode node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrderTraversal(node.left);
+
+        if (prev != null) {
+            minDiff = Math.min(minDiff, node.val - prev.val);
+        }
+
+        prev = node;
+
+        inOrderTraversal(node.right);
+    }
 
 
 
