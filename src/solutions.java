639a129c09a649731e587f450a9613c80ee797b3
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-07-31 12:45:00 +0200

    containsNearbyDuplicate

--- src/solutions.java
+++ src/solutions.java
@@ -1816,7 +1816,20 @@ public class solutions {
         return true;
     }
 
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
 
+        HashMap<Integer,Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i]) && Math.abs(map.get(nums[i])-i)<=k){
+                return true;
+            }
+            else{
+                map.put(nums[i],i);
+            }
+        }
+        return false;
+    }
 
 
 
