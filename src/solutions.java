fe8d874e186784792d28caeb665e6fe16beac2f4
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-02 15:09:03 +0200

    zigzagLevelOrder

--- src/solutions.java
+++ src/solutions.java
@@ -2232,7 +2232,38 @@ public class solutions {
 
 
 
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List<List<Integer>> ans = new ArrayList<>();
+        if (root == null) return ans;
 
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        boolean zigzag = false;
+        while (!queue.isEmpty()){
+            List<Integer> allNodes = new ArrayList<>();
+
+            int size = queue.size();
+            for (int i = 0; i < size; i++){
+
+                TreeNode current = queue.poll();
+                allNodes.add(current.val);
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+
+
+            }
+            if (ans.size() % 2 == 1){
+                Collections.reverse(allNodes);
+            }
+            ans.add(allNodes);
+
+
+
+        }
+
+        return ans;
+    }
 
 
 
