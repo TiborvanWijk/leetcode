99d04680fda7c7206c0e68c0a9791da4df75a0be
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-10 21:12:43 +0200

    postorderTraversal

--- src/solutions.java
+++ src/solutions.java
@@ -2473,8 +2473,16 @@ public class solutions {
 //    }
 
 
-
-
+//    List<Integer> list = new ArrayList<>();
+//    public List<Integer> postorderTraversal(TreeNode root) {
+//        if (root != null){
+//            postorderTraversal(root.left);
+//            postorderTraversal(root.right);
+//            list.add(root.val);
+//        }
+//
+//        return list;
+//    }
 
 
 
