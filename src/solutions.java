4fe4ecdeb96afc48b36b997f9315167b46780830
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-10 20:28:28 +0200

    Update solutions.java

--- src/solutions.java
+++ src/solutions.java
@@ -2425,6 +2425,24 @@ public class solutions {
 
 
 
+//    List<Integer> list = new ArrayList<>();
+//    public List<Integer> inorderTraversal(TreeNode root) {
+//
+//        if (root != null){
+//            inorderTraversal(root.left);
+//            list.add(root.val);
+//            inorderTraversal(root.right);
+//        }
+//
+//        return list;
+//    }
+
+
+
+
+
+
+
 
 
 
