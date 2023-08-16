fe451a08a65e608c72fc382dc14e837de3ee05a9
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-16 16:36:19 +0200

    findMaxConsecutiveOnes

--- src/solutions.java
+++ src/solutions.java
@@ -2899,7 +2899,23 @@ public class solutions {
 
 
 
+    public int findMaxConsecutiveOnes(int[] nums) {
 
+        int ans = 0;
+        int consecutive = 0;
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] == 1){
+                consecutive++;
+            }
+            else {
+                consecutive = 0;
+            }
+            ans = Math.max(ans, consecutive);
+
+        }
+
+        return ans;
+    }
 
 
 
