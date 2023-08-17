5e4d5d23eb34b2c371424537766d93df9620887e
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-17 12:53:18 +0200

    trailingZeroes

--- src/solutions.java
+++ src/solutions.java
@@ -3102,7 +3102,14 @@ public class solutions {
 
 
 
-
+    public int trailingZeroes(int n) {
+        int count = 0;
+        while(n > 0) {
+            n /= 5;
+            count += n;
+        }
+        return count;
+    }
 
 
 
