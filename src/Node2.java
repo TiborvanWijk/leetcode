5446c7d982db59c93e40e4372b3e7f763fd1eda6
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-18 18:12:46 +0200

    mergeAlternately

---
+++ src/Node2.java
@@ -0,0 +1,13 @@
+class Node2 {
+    int val;
+    Node2 left;
+    Node2 right;
+    Node2 next;
+
+    Node2(int val) {
+        this.val = val;
+        left = null;
+        right = null;
+        next = null;
+    }
+}
--- src/solutions.java
+++ src/solutions.java
@@ -3263,6 +3263,31 @@ public class solutions {
 
 
 
+    public String mergeAlternately(String word1, String word2) {
+        StringBuilder ans = new StringBuilder();
+        int i = 0;
+        while (i < word1.length() && i < word2.length()){
+
+            ans.append(word1.charAt(i));
+            ans.append(word2.charAt(i));
+
+            i++;
+        }
+        if (i < word1.length()) ans.append(word1.substring(i));
+        if (i < word2.length()) ans.append(word2.substring(i));
+
+
+        return ans.toString();
+    }
+
+
+
+
+
+
+
+
+
 
 
 
