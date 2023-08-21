5df4f6eb8b3ac97d7519d3aab5a76cf5c6c65aee
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-21 14:22:39 +0200

    maxLevelSum

--- src/solutions.java
+++ src/solutions.java
@@ -3491,6 +3491,38 @@ public class solutions {
 
 
 
+    public int maxLevelSum(TreeNode root) {
+
+        int max = Integer.MIN_VALUE;
+        int lvl = 0;
+        int ans = 0;
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+            int lvlSum = 0;
+            lvl++;
+            for (int i = 0; i < size; i++){
+
+                TreeNode node = queue.poll();
+                lvlSum += node.val;
+
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+
+            }
+
+            if (lvlSum > max){
+                max = lvlSum;
+                ans = lvl;
+            }
+
+        }
+        return ans;
+    }
 
 
 
