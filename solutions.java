af219267dd4a6a1412015c0dc835ac3d42868753
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-30 10:30:30 +0200

    addTwoNumbers

--- solutions.java
+++ solutions.java
@@ -4233,4 +4233,53 @@ public class solutions {
 
 
 
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+        ListNode dummy = new ListNode();
+        ListNode current = dummy;
+
+        int carry = 0;
+
+        while (l1 != null || l2 != null || carry != 0){
+
+            int val1 = l1 != null ? l1.val : 0;
+            int val2 = l2 != null ? l2.val : 0;
+
+            int val = val1 + val2 + carry;
+            carry = val / 10;
+            val = val % 10;
+
+            current.next = new ListNode(val);
+
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+
+
+            current = current.next;
+
+        }
+
+        return dummy.next;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
