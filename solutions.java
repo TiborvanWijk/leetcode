f3abeb53ce92769dbf31f6812d705067224b69c6
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-31 16:52:11 +0200

    accountBalanceAfterPurchase

--- solutions.java
+++ solutions.java
@@ -4527,7 +4527,17 @@ public class solutions {
 
 
 
+    public int accountBalanceAfterPurchase(int purchaseAmount) {
 
+        if (purchaseAmount % 10 >= 5){
+            purchaseAmount += 10 - (purchaseAmount % 10);
+        }
+        else {
+            purchaseAmount -= purchaseAmount % 10;
+        }
+
+        return 100 - purchaseAmount;
+    }
 
 
 
