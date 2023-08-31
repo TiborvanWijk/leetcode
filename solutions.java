60bdc4cddbb11817531e84ae929d3678e332f454
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-08-31 18:19:54 +0200

    judgeCircle

--- solutions.java
+++ solutions.java
@@ -4597,6 +4597,34 @@ public class solutions {
 
 
 
+    public boolean judgeCircle(String moves) {
+
+        int vertical = 0;
+        int horizontal = 0;
+
+        for (int i = 0; i < moves.length(); i++){
+
+            switch (moves.charAt(i)){
+                case 'U':
+                    vertical++;
+                    break;
+                case 'D':
+                    vertical--;
+                    break;
+                case 'R':
+                    horizontal++;
+                    break;
+                case 'L':
+                    horizontal--;
+                    break;
+            }
+        }
+
+
+        return vertical == 0 && horizontal == 0;
+    }
+
+
 
 
 
