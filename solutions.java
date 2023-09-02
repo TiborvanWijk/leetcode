34397deb2dc73b494079fa36f62ebbb474e5c6d1
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-02 12:46:45 +0200

    reverseBetween

--- solutions.java
+++ solutions.java
@@ -4715,6 +4715,26 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode prev = dummy;
+
+        for(int i = 0; i < left - 1; i++)
+            prev = prev.next;
+
+        ListNode curr = prev.next;
+        // reversing
+        for(int i = 0; i < right - left; i++){
+            ListNode forw = curr.next;
+            curr.next = forw.next;
+            forw.next = prev.next;
+            prev.next = forw;
+        }
+        return dummy.next;
+    }
+
+
 
 
 
