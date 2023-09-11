5be72b17ac0c6fc890730ae34de05091f8549f6c
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-11 14:54:35 +0200

    largestValues

--- solutions.java
+++ solutions.java
@@ -5344,6 +5344,36 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public List<Integer> largestValues(TreeNode root) {
+
+        List<Integer> ans = new ArrayList<>();
+        if (root == null) return ans;
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+            int max = Integer.MIN_VALUE;
+            for (int i = 0; i < size; i++){
+
+                TreeNode current = queue.poll();
+                if (current.val > max){
+                    max = current.val;
+                }
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+            }
+            ans.add(max);
+
+        }
+
+
+        return ans;
+    }
+
 
 
 
