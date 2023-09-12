1a9283de1f023a68f2a0c2b0f10069c823288171
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-12 10:31:22 +0200

    findBottomLeftValue

--- solutions.java
+++ solutions.java
@@ -1,3 +1,5 @@
+import com.sun.source.tree.Tree;
+
 import java.util.*;
 
 /**
@@ -5377,7 +5379,34 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int findBottomLeftValue(TreeNode root) {
 
+        int leftmost = 0;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+
+        while (!queue.isEmpty()){
+
+            int size = queue.size();
+
+            for (int i = 0; i < size; i++){
+
+                TreeNode current = queue.poll();
+                if (i == 0){
+                    leftmost = current.val;
+                }
+
+
+                if (current.left != null) queue.offer(current.left);
+                if (current.right != null) queue.offer(current.right);
+
+            }
+        }
+
+        return leftmost;
+    }
 
 
 
