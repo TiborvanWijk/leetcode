02ee240575f09d61d7e366f5799ccda066a4a601
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-12 13:21:45 +0200

    matrixReshape

--- solutions.java
+++ solutions.java
@@ -5492,7 +5492,39 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int[][] matrixReshape(int[][] mat, int r, int c) {
 
+        if (mat.length * mat[0].length > r * c){
+            return mat;
+        }
+
+        int[][] ans = new int[r][c];
+
+        int row = 0;
+        int column = 0;
+
+        for (int i = 0; i < mat.length; i++){
+
+            for (int j = 0; j < mat[0].length; i++){
+
+                if (column >= c){
+                    column = 0;
+                    row++;
+                }
+
+
+                ans[row][column] = mat[i][j];
+                column++;
+
+
+            }
+
+        }
+
+
+        return ans;
+
+    }
 
 
 
