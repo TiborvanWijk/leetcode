7c6d12b605948cfd3812cd3e17b3b264417bf92c
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-13 15:53:35 +0200

    averageValue

--- solutions.java
+++ solutions.java
@@ -5691,7 +5691,26 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int averageValue(int[] nums) {
 
+        int ans = 0;
+        int amount = 0;
+
+        for (int i = 0; i < nums.length; i++){
+
+            if (nums[i] % 2 == 0 && nums[i] % 3 == 0){
+                ans += nums[i];
+                amount++;
+            }
+
+        }
+
+        if (amount == 0){
+            return 0;
+        }
+
+        return  ans / amount;
+    }
 
 
 
