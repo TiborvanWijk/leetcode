f965869f5c167aaff4c9b7368be0ca1819583527
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-14 19:05:06 +0200

    maxAscendingSum

--- solutions.java
+++ solutions.java
@@ -5918,7 +5918,32 @@ public boolean judgeCircle(String moves) {
 
     }
 
+    public int maxAscendingSum(int[] nums) {
 
+        if (nums.length == 1) return nums[0];
+
+        int max = 0;
+        int current = nums[0];
+        for (int i = 1; i < nums.length; i++){
+
+            if (nums[i] > nums[i- 1]){
+                current += nums[i];
+            }
+            else {
+                if (current > max){
+                    max = current;
+                }
+                current = nums[i];
+            }
+
+
+        }
+        if (current > max){
+            max = current;
+        }
+
+        return max;
+    }
 
 
 
