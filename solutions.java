75d283e97f6a3006af5d067cd5158edbd9823a46
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-16 11:07:09 +0200

    squareIsWhite

--- solutions.java
+++ solutions.java
@@ -6157,7 +6157,30 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public boolean squareIsWhite(String coordinates) {
+        Map<Character, Integer> map = new HashMap<>();
 
+        map.put('a',0);
+        map.put('b',1);
+        map.put('c',2);
+        map.put('d',3);
+        map.put('e',4);
+        map.put('f',5);
+        map.put('g',6);
+        map.put('h',7);
+
+        int nr1 = map.get(coordinates.charAt(0));
+        int nr2 = coordinates.charAt(1);
+
+        if (nr1 % 2 == 0 && nr2 % 2 == 0){
+            return true;
+        }
+        else if (nr1 % 2 == 1 && nr2 % 2 == 1){
+            return true;
+        }
+
+        return false;
+    }
 
 
 
