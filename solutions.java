40b1e8f99a2711d290f4865a82d93f430dd1032f
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-17 12:51:12 +0200

    heightChecker

--- solutions.java
+++ solutions.java
@@ -6365,10 +6365,25 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int heightChecker(int[] heights) {
+
+        int ans = 0;
 
 
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
 
+        for (int i = 0; i < heights.length; i++){
 
+            if (heights[i] != expected[i]){
+                ans++;
+            }
+
+        }
+
+        return ans;
+
+    }
 
 
 
