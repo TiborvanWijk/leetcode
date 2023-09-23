6a26a91eee2073e9bd0d18080610bec5c6c01781
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-23 14:14:21 +0200

    smallestRepunitDivByK

--- solutions.java
+++ solutions.java
@@ -7103,7 +7103,18 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int smallestRepunitDivByK(int k) {
+        if(k % 2 == 0 || k % 5 == 0) return -1;
 
+        int number = 0;
+        for (int i = 1; i <= k; i++){
+
+            number = ((number*10 + 1)%k);
+            if (number == 0) return i;
+
+        }
+        return -1;
+    }
 
 
 
