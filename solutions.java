2b99895558dcb5a601e111071f5d7c1de23b8d02
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-26 13:10:01 +0200

    79. Word Search

--- solutions.java
+++ solutions.java
@@ -7246,22 +7246,6 @@ public boolean judgeCircle(String moves) {
         dfs(grid, i, j-1, steps + 1, visited);
     }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     int goodNodes = 0;
     public int goodNodes(TreeNode root) {
 
@@ -7333,9 +7317,41 @@ public boolean judgeCircle(String moves) {
 
 
 
+//79. Word Search
+    public boolean exist(char[][] board, String word) {
+        boolean[][] seen = new boolean[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++){
+            for (int j = 0; j < board[0].length; j++){
+                if (board[i][j] == word.charAt(0)){
+                    if (dfs(board, i, j, 0, word)){
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
 
+    public boolean dfs(char[][] board, int i, int j, int start, String word) {
+        if (start == word.length()){
+            return true;
+        }
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || board[i][j] != word.charAt(start)){
+            return false;
+        }
 
+        char temp = board[i][j];
+        board[i][j] = ' ';
 
+        boolean found = dfs(board, i+1, j, start +1,word)||
+                dfs(board, i-1, j, start+1,word)||
+                dfs(board, i, j+1, start+1,word)||
+                dfs(board, i, j-1, start+1,word);
+
+        board[i][j] = temp;
+
+        return found;
+    }
 
 
 
