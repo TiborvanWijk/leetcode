cb136204c95386e5d3a70933c2b4ab6cc7dddd52
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-09-28 16:32:19 +0200

    copyRandomList

--- solutions.java
+++ solutions.java
@@ -7664,8 +7664,43 @@ public boolean judgeCircle(String moves) {
 
 
 
-
-
+//    class Node {
+//        int val;
+//        Node next;
+//        Node random;
+//
+//        public Node(int val) {
+//            this.val = val;
+//            this.next = null;
+//            this.random = null;
+//        }
+//    }
+//    public Node copyRandomList(Node head) {
+//        if(head == null) return null;
+//
+//        HashMap<Node, Node> map = new HashMap<>();
+//        map.put(null, null);
+//
+//        Node curr = head;
+//
+//
+//        while (curr != null){
+//
+//            Node copy = new Node(curr.val);
+//            map.put(curr, copy);
+//            curr = curr.next;
+//        }
+//        curr = head;
+//        while (curr != null){
+//
+//            Node copy = map.get(curr);
+//            copy.next = map.get(curr.next);
+//            copy.random = map.get(curr.random);
+//            curr = curr.next;
+//        }
+//
+//        return map.get(head);
+//    }
 
 
 
