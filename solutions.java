b2ce3ffc2261e0849d91671f997e0c53ac88b3a0
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-02 15:15:30 +0200

    countCollisions

--- solutions.java
+++ solutions.java
@@ -8628,7 +8628,23 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int countCollisions(String directions) {
 
+        int left = 0;
+        int right = directions.length() - 1;
+        while(left < directions.length() && directions.charAt(left) == 'L'){
+            left++;
+        }
+        while(right >= 0 && directions.charAt(right) == 'R'){
+            right--;
+        }
+
+        int res = 0;
+        for(int i=left; i<=right; ++i) {
+            if(directions.charAt(i) != 'S')    res++;
+        }
+        return res;
+    }
 
 
 
