05676fd44e8c5d4817df46dab1b5e470489a1afa
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-06 22:07:16 +0200

    firstMissingPositive

--- solutions.java
+++ solutions.java
@@ -9367,7 +9367,35 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int firstMissingPositive(int[] nums) {
+        for (int i = 0; i < nums.length; i++){
+            if (nums[i] <= 0 || nums[i] > nums.length) {
+                nums[i] = nums.length + 1;
+            }
+        }
 
+
+        for (int i = 0; i < nums.length; i++){
+            int index = Math.abs(nums[i]);
+            if (index > 0 && index <= nums.length){
+
+                if (nums[index-1] == 0){
+                    nums[index-1] = -1 * nums.length+1;
+                }
+                if (nums[index-1] > 0){
+                    nums[index-1] = -Math.abs(nums[index-1]);
+                }
+            }
+        }
+
+        for (int i = 1; i <= nums.length; i++){
+            if (nums[i-1] >= 0){
+                return i;
+            }
+        }
+
+        return nums.length+1;
+    }
 
 
 
