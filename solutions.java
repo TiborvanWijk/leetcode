69b7ffef65c74fc6359208f0856fe783373d5de5
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-10 17:41:52 +0200

    minOperations

--- solutions.java
+++ solutions.java
@@ -10081,7 +10081,28 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public int minOperations(int[] nums) {
 
+        Arrays.sort(nums);
+        int unique = 1;
+        for (int i = 1; i < nums.length; ++i) {
+            if (nums[i] != nums[i - 1]) {
+                nums[unique] = nums[i];
+                unique++;
+            }
+        }
+
+        int ans = nums.length;
+        for (int i = 0, j = 0; i < unique; i++) {
+            while (j < unique && nums[j] - nums[i] <= nums.length - 1) {
+                j++;
+            }
+            ans = Math.min(ans, nums.length - (j - i));
+        }
+
+        return ans;
+
+    }
 
 
 
