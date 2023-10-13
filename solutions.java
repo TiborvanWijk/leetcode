4a84decfb7c407c9174f9d4d4872aad87498ebc5
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-13 16:26:38 +0200

    partition

--- solutions.java
+++ solutions.java
@@ -10486,4 +10486,56 @@ public boolean judgeCircle(String moves) {
     }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public List<List<String>> partition(String s) {
+        List<List<String>> ans = new ArrayList<>();
+        List<String> curr = new ArrayList<>();
+
+        backTrackPalidrone(s,0, curr, ans);
+
+        return ans;
+    }
+
+    private void backTrackPalidrone(String s, int index, List<String> list, List<List<String>> ans) {
+
+        if (index == s.length()){
+            ans.add(new ArrayList<>(list));
+            return;
+        }
+
+
+        for (int i = index; i < s.length(); i++){
+            if (isPalindrome(s, index, i)){
+                list.add(s.substring(index, i+1));
+                backTrackPalidrone(s, i+1, list, ans);
+                list.remove(list.size()-1);
+            }
+        }
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+
+        while (left < right){
+            if (s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+
 }
