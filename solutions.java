d74fc750f9cd1bba569c1dc98618c5dd48c7b4e2
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-14 18:45:09 +0200

    nextPermutation

--- solutions.java
+++ solutions.java
@@ -10846,47 +10846,46 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public void nextPermutation(int[] nums) {
 
+        int index = -1;
+        for (int i = nums.length-1; i > 0; i--){
 
+            if (nums[i] > nums[i-1]){
+                index = i-1;
+                break;
+            }
+        }
 
+        if (index == -1){
+            reverseArray(nums, 0, nums.length-1);
+            return;
+        }
+        else {
 
+            for (int i = nums.length-1; i >= 0; i--){
+                if (nums[i] > nums[index]){
+                    swap(nums, i, index);
+                    break;
+                }
+            }
+            reverseArray(nums, index+1, nums.length-1);
+        }
+    }
 
+    private void reverseArray(int[] nums, int i, int j) {
+        while (i < j){
+            swap(nums, i, j);
+            i++;
+            j--;
+        }
+    }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
 
 
 }
