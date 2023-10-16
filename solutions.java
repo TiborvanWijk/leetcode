bd850621bbf845bcf2a4cf8c78e523b49f1c340e
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-16 17:12:25 +0200

    getrow pascals triangle

--- solutions.java
+++ solutions.java
@@ -10970,7 +10970,18 @@ public boolean judgeCircle(String moves) {
 
 
 
+    public List<Integer> getRow(int rowIndex) {
+        List<Integer> row = new ArrayList<>();
+        long ans = 1;
 
+        for(int i = 0;i<=rowIndex;i++){
+            row.add((int)ans);
+            ans = ans * (rowIndex-i);
+            ans = ans / (i+1);
+        }
+
+        return row;
+    }
 
 
 
