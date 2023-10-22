4794abf8746c1b77efc652a200d9e6de83add84d
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-22 12:24:38 +0200

    1670. Design Front Middle Back Queue

--- solutions.java
+++ solutions.java
@@ -11379,8 +11379,49 @@ public class solutions {
 
 
 
+    List<Integer> list;
+    public FrontMiddleBackQueue() {
+        list = new ArrayList<>();
+    }
 
+    public void pushFront(int val) {
+        list.add(0,val);
+    }
 
+    public void pushMiddle(int val) {
+        list.add(list.size()/2, val);
+    }
+
+    public void pushBack(int val) {
+        list.add(val);
+    }
+
+    public int popFront() {
+        if (list.size() == 0)
+            return -1;
+
+        int ans = list.get(0);
+        list.remove(0);
+        return ans;
+    }
+
+    public int popMiddle() {
+        if (list.size() == 0)
+            return -1;
+
+        int ans = list.get((list.size()-1)/2);
+        list.remove((list.size()-1)/2);
+        return ans;
+    }
+
+    public int popBack() {
+        if (list.size() == 0)
+            return -1;
+
+        int ans = list.get(list.size()-1);
+        list.remove(list.size()-1);
+        return ans;
+    }
 
 
 
