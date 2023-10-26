9949ec882b4ebc44d5879ea20992ea571cb81ce4
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-26 13:12:44 +0200

    1476. Subrectangle Queries

--- solutions.java
+++ solutions.java
@@ -11592,22 +11592,6 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// try again!!!!!!!!!!!!
     public int kthGrammar(int n, int k) {
         if (n == 1) {
             return 0;
@@ -11625,10 +11609,26 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
+//1476. Subrectangle Queries
+    int[][] rectangle;
 
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
 
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
 
+        for (int i = row1; i <= row2; i++){
+            for (int j = col1; j <= col2; j++){
+                rectangle[i][j] = newValue;
+            }
+        }
 
+    }
+
+    public int getValue(int row, int col) {
+        return rectangle[row][col];
+    }
 
 
 
