b8e9fb3250e24e04f1e9552958684a34e9baf941
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-27 13:18:51 +0200

    2482. Difference Between Ones and Zeros in Row and Column

--- solutions.java
+++ solutions.java
@@ -12020,8 +12020,24 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
+//2482. Difference Between Ones and Zeros in Row and Column
+    public int[][] onesMinusZeros(int[][] grid) {
+        int[] rowDiff = new int[grid.length];
+        int[] colDiff = new int[grid[0].length];
+        for (int i = 0; i < grid.length; i++){
+            for (int j = 0; j < grid[0].length; j++){
+                rowDiff[i] += 1-(2*grid[i][j]);
+                colDiff[j] += 1-(2*grid[i][j]);
+            }
+        }
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                grid[i][j] = -rowDiff[i] + -colDiff[j];
+            }
+        }
 
-
+        return grid;
+    }
 
 
 
