c3df5a4f77db0aa5c30d37c6c88e9f9afa607932
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-10-31 14:46:16 +0100

    xor last daily for badge

--- solutions.java
+++ solutions.java
@@ -12303,7 +12303,16 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
+    public int[] findArray(int[] pref) {
+        int[] ans = new int[pref.length];
+        ans[0] = pref[0];
 
+        for (int i = 1; i < pref.length; i++){
+            ans[i] = pref[i] ^ pref[i-1];
+        }
+
+        return ans;
+    }
 
 
 
