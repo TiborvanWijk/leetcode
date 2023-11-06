97be5ce8e35fc3bb2d8a284ea17f391c5e319649
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-11-06 17:21:54 +0100

    2428. Maximum Sum of an Hourglass

--- solutions.java
+++ solutions.java
@@ -12596,154 +12596,27 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//2428. Maximum Sum of an Hourglass
+    public int maxSum(int[][] grid) {
+        int max = 0;
+        for (int i = 0; i < grid.length-2; i++){
+            for (int j = 0; j < grid[0].length-2; j++){
+                max = Math.max(max, hourglassSum(grid, i,j));
+            }
+        }
+
+        return max;
+    }
+
+    private int hourglassSum(int[][] grid, int i, int j) {
+        return grid[i][j] +
+        grid[i][j+1] +
+        grid[i][j+2] +
+        grid[i+1][j+1] +
+        grid[i+2][j+1] +
+        grid[i+2][j+2] +
+        grid[i+2][j];
+    }
 
 
 
