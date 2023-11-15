21907c1efdbb74ca1a551598cedb256dbf5413e3
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-11-15 11:14:04 +0100

    1846. Maximum Element After Decreasing and Rearranging

--- solutions.java
+++ solutions.java
@@ -13243,4 +13243,61 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
     }
 
 
+
+
+
+
+
+
+//1846. Maximum Element After Decreasing and Rearranging
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        arr[0] = 1;
+        for (int i = 1; i < arr.length; i++){
+            if (arr[i-1]+1 < arr[i]){
+                arr[i] = arr[i-1]+1;
+            }
+        }
+        return arr[arr.length-1];
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
