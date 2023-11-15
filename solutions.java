31b758ae80280d0e4b97a1577a229fc7be3a115b
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-11-15 14:05:49 +0100

    540. Single Element in a Sorted Array

--- solutions.java
+++ solutions.java
@@ -13344,8 +13344,37 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
     }
 
 
+//540. Single Element in a Sorted Array
+    public int singleNonDuplicate(int[] nums) {
+        int left = 0;
+        int right = nums.length-1;
 
 
+        while (left <= right){
+
+            int mid = (left + right) / 2;
+
+            if (mid > 0 && mid < nums.length-1){
+
+                if (nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1]){
+                    return nums[mid];
+                }
+                else if (mid % 2 == 0 && nums[mid] != nums[mid+1] || mid % 2 == 1 && nums[mid] != nums[mid-1]){
+                    right = mid-1;
+                }
+                else if (mid % 2 == 0 && nums[mid] == nums[mid+1] || mid % 2 == 1 && nums[mid] == nums[mid-1]){
+                    left = mid+1;
+                }
+            }
+            else {
+                return nums[mid];
+            }
+
+
+        }
+
+        return nums[left];
+    }
 
 
 
