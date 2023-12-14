46bb83ca5b916c598431d8c3a0b0ff16d8318548
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2023-12-14 19:43:44 +0100

    1637. Widest Vertical Area Between Two Points Containing No Points

--- solutions.java
+++ solutions.java
@@ -14462,8 +14462,24 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
+//1637. Widest Vertical Area Between Two Points Containing No Points
+    public int maxWidthOfVerticalArea(int[][] points) {
+
+        int[] x = new int[points.length];
 
 
+        for (int i = 0; i < points.length; ++i){
+            x[i] = points[i][0];
+        }
+
+        Arrays.sort(x);
+        int max = 0;
+
+        for (int i = 1; i < points.length; ++i){
+            max = Math.max(max, x[i]-x[i-1]);
+        }
+        return max;
+    }
 
 
 
