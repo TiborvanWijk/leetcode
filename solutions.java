687285486b6d0f72e074d5af4a5b12298417d31f
Tibor van Wijk <137512140+TiborvanWijk@users.noreply.github.com>
2024-01-01 12:12:19 +0100

    455. Assign Cookies

--- solutions.java
+++ solutions.java
@@ -14706,6 +14706,55 @@ public List<List<Integer>> queensAttacktheKing(int[][] queens, int[] king) {
 
 
 
+//455. Assign Cookies
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int ans = 0;
+        int childIndex = 0;
+        int cookieIndex = 0;
+        while (childIndex < g.length && cookieIndex < s.length){
+
+            if (g[childIndex] <= s[cookieIndex]){
+                ++ans;
+                ++childIndex;
+            }
+            ++cookieIndex;
+        }
+        return ans;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
